c7afb7163009d6e846ffa2a7d0a9cc28062a9893
Denis Retinsky <godasin@gmail.com>
2022-06-09 12:38:30 +0300

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
@@ -19,7 +20,7 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [new webpack.HotModuleReplacementPlugin(), new ESLintPlugin()],
   devServer: {
     static: path.resolve(__dirname, './dist'),
     hot: true,
